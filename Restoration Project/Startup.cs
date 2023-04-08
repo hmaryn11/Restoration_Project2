7bbb1d3f04853a4c35ecddfb74e16cf24c3be0b7
hmaryn@byu.edu <95374695+hmaryn11@users.noreply.github.com>
2023-04-08 12:05:27 -0600

    resource and navbar changes

--- Restoration Project/Startup.cs
+++ Restoration Project/Startup.cs
@@ -59,7 +59,7 @@ namespace Restoration_Project
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=JeopardyHome}/{id?}");
             });
         }
     }
